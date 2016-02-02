6fa8062243dc8f39540fb3339c61aa21c92d54f9
Eric Hamiter <eric@artistgrowth.com>
2016-02-02 08:07:42 -0600

    organizing into a package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PyUFC',
+    version='0.1',
+    packages=['pyufc', ],
+    license='The MIT License',
+    long_description=open('README.md').read(),
+)
